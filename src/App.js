ed233678758bc5970b8e08ff68550cf5e86aa994
SofireOfficial <soph.johns247@gmail.com>
2022-01-17 21:30:52 +0000

    intial edits

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Coffee Matches
+        </p>
+        <a
+          className="App-link"
+          href="https://www.coffeematches.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Referral Hub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
