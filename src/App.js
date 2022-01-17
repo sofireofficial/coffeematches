9dd12177851e581135ce55771addb6d2c45243d1
SofireOfficial <soph.johns247@gmail.com>
2022-01-17 22:56:19 +0000

    quick update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from 'react'
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Header from './components/Header';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Referees from './pages/Referees';
 
-function App() {
-  return (
+function App() { 
+  return ( 
     <div className="App">
       <header className="App-header">
-        <p>
+        <h1>
           Coffee Matches
-        </p>
+        </h1>
         <a
           className="App-link"
           href="https://www.coffeematches.com/"
@@ -15,7 +21,22 @@ function App() {
         >
           Referral Hub
         </a>
-      </header>
+      {/* <Router> */}
+          {/* <Header />
+          <Switch>
+
+            <Route exact path="/"></Route> */}
+            <Home />
+
+            {/* <Route path="/about"><About /></Route> */}
+            <About />
+
+            {/* <Route path="/referees"><Referees /></Route> */}
+            <Referees />
+
+          {/* </Switch>
+        </Router> */}
+        </header>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+return (
+    <div>
+    <h1>⭐Home⭐</h1>
+    <p style={{color:"#775b1d"}}>This is the home page</p>
+    </div>
+);
+};
+
+export default Home
+
+
